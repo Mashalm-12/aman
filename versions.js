2b8c818a812078ff9a1a8d590f5149acc88f9d4f
mashalmohammed11@gmail.com <113477330+Mashalm-12@users.noreply.github.com>
2026-03-16 03:36:57 +0300

    Add files via upload

---
+++ versions.js
@@ -0,0 +1,139 @@
+// versions.js
+const VERSIONS = {
+  "الشامل": [
+    {
+      oracle: "19",
+      type: "الشامل",
+      name: "AL SHAAMEL",
+      version: "10.0.6",
+      update: "351",
+      releaseDate: "2026-03-01",
+      desc: "نسخة الشامل Oracle 19",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/drive/folders/1MXH2yJIoMK0M3_mv7sr-eqlLUrTma_xj?usp=drive_link",
+      status: "new"
+    },
+    {
+      oracle: "19",
+      type: "الشامل",
+      name: "الشامل",
+      version: "6.0",
+      update: "5",
+      releaseDate: "2025-09-12",
+      desc: "نسخة الشامل القديمة Oracle 19",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link2",
+      status: "old"
+    },{
+      oracle: "19",
+      type: "الشامل",
+      name: "الشامل",
+      version: "6.0",
+      update: "5",
+      releaseDate: "2025-09-12",
+      desc: "نسخة الشامل القديمة Oracle 19",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link2",
+      status: "new"
+    },{
+      oracle: "19",
+      type: "الشامل",
+      name: "الشامل",
+      version: "6.0",
+      update: "5",
+      releaseDate: "2025-09-12",
+      desc: "نسخة الشامل القديمة Oracle 19",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link2",
+      status: "new"
+    }, {
+      oracle: "19",
+      type: "الشامل",
+      name: "الشامل",
+      version: "6.0",
+      update: "5",
+      releaseDate: "2025-09-12",
+      desc: "نسخة الشامل القديمة Oracle 19",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link2",
+      status: "old"
+    },{
+      oracle: "19",
+      type: "الشامل",
+      name: "الشامل",
+      version: "6.0",
+      update: "5",
+      releaseDate: "2025-09-12",
+      desc: "نسخة الشامل القديمة Oracle 19",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link2",
+      status: "new"
+    },
+    {
+      oracle: "10",
+      type: "الشامل",
+      name: "الشامل",
+      version: "6.0",
+      update: "4",
+      releaseDate: "2024-11-20",
+      desc: "نسخة الشامل Oracle 10",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link3",
+      status: "old"
+    }
+  ],
+
+  "الشامل بلاس": [
+    {
+      oracle: "19",
+      type: "الشامل بلاس",
+      name: "الشامل بلاس",
+      version: "6.0",
+      update: "3",
+      releaseDate: "2026-01-15",
+      desc: "نسخة الشامل بلاس Oracle 19",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link4",
+      status: "new"
+    },
+    {
+      oracle: "10",
+      type: "الشامل بلاس",
+      name: "الشامل بلاس",
+      version: "6.0",
+      update: "2",
+      releaseDate: "2025-08-10",
+      desc: "نسخة الشامل بلاس Oracle 10",
+      pos: "POS: إي تي جوال / ديسك توب",
+      url: "https://drive.google.com/link5",
+      status: "old"
+    }
+  ],
+
+  "الأولتكس": [
+    {
+      oracle: "19",
+      type: "الأولتكس",
+      name: "الأولتكس",
+      version: "2.0",
+      update: "1",
+      releaseDate: "2026-02-05",
+      desc: "نسخة الأولتكس Oracle 19",
+      pos: "POS: مدير الشركة فقط",
+      url: "https://drive.google.com/link6",
+      status: "new"
+    },
+    {
+      oracle: "10",
+      type: "الأولتكس",
+      name: "الأولتكس",
+      version: "2.0",
+      update: "1",
+      releaseDate: "2025-07-18",
+      desc: "نسخة الأولتكس Oracle 10",
+      pos: "POS: مدير الشركة فقط",
+      url: "https://drive.google.com/link7",
+      status: "old"
+    }
+  ]
+};
